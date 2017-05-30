ac44e6e02f40bb179b5b92593ceb656bf1b3968c
HannaSokolsky <hannasokolsky@gmail.com>
2017-05-30 11:41:01 -0700

    2 commit

--- src/test/java/core/AppTest.java
+++ src/test/java/core/AppTest.java
@@ -3,7 +3,11 @@ package core;
 import org.testng.annotations.Test;
 
 public class AppTest {
-  @Test
-  public void f() {
+	
+	
+  @Test(enabled =true)
+  public void test() {
+	  
+	  org.testng.Assert.assertEquals("abc", "abc", "Error message ");
   }
 }
